fe3b61c35ff73fa31ae78b13128216415278b4f0
GetieBalew24 <getiebalew2020@gmail.com>
2023-03-29 12:41:27 -0700

    0x01. Lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,24 +1,21 @@
 #!/usr/bin/python3
-""" Display  locked boxes in front of you"""
+'''A module for working with lockboxes.
+'''
 
 
 def canUnlockAll(boxes):
-    """Define unlock array of boxes"""
-    unlocked = set([0]
-    key = set(boxes[0]
-    while True:
-        next_box = None
-        for i in range(len(boxes)):
-            if i not in unlocked and set(boxes[i]).intersection(key):
-                next_box = i
-                break
-        if next_box is None:
-            break
-        else:
-            unlocked.add(next_box)
-            key.update(boxes[next_box])
-
-    if len(unlocked) == len(boxes)
-        return true
-    else:
-    return false
+    '''Checks if all the boxes in a list of boxes containing the keys
+    (indices) to other boxes can be unlocked given that the first
+    box is unlocked.
+    '''
+    n = len(boxes)
+    seen_boxes = set([0])
+    unseen_boxes = set(boxes[0]).difference(set([0]))
+    while len(unseen_boxes) > 0:
+        boxIdx = unseen_boxes.pop()
+        if not boxIdx or boxIdx >= n or boxIdx < 0:
+            continue
+        if boxIdx not in seen_boxes:
+            unseen_boxes = unseen_boxes.union(boxes[boxIdx])
+            seen_boxes.add(boxIdx)
+    return n == len(seen_boxes)
