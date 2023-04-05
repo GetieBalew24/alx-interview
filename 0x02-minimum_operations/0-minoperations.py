c8a8ea85a63184ff872928e7aa0e84e4d03b6c4e
pc <getiebalew2020@gmail.com>
2023-04-05 15:07:23 +0300

    methods to calculate the fewest number of operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,16 +1,25 @@
 #!/usr/bin/python3
-'''0x02. Minimum Operations Algorithm Python in a text
-'''
+""" Module for 0-minoperations"""
 
 
 def minOperations(n):
-    '''calculates the fewest number of operations.
-    '''
-    total_operation = 0
-    total_value = 2
-    while n > 1:
-        while not n % total_value:
-            total_operation += total_value
-            n /= total_value
-        total_value += 1
-    return total_operation
+    """
+    minOperations
+    Gets fewest # of operations needed to result in exactly n H characters
+    """
+    # all outputs should be at least 2 char: (min, Copy All => Paste)
+    if (n < 2):
+        return 0
+    ops, root = 0, 2
+    while root <= n:
+        # if n evenly divides by root
+        if n % root == 0:
+            # total even-divisions by root = total operations
+            ops += root
+            # set n to the remainder
+            n = n / root
+            # reduce root to find remaining smaller vals that evenly-divide n
+            root -= 1
+        # increment root until it evenly-divides n
+        root += 1
+    return ops
