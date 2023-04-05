b231ef5892b520650d53752b5c7ce72282be0e17
pc <getiebalew2020@gmail.com>
2023-04-05 15:39:40 +0300

    methods to calculate the fewest number of operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -6,14 +6,13 @@ def minOperations(n):
     ''' calculates number operations '''
     # all outputs have at least 2 letters
     total_operation = 0
-    root_value = 2
-    while n > 1:
+    for root_value in range(2, n+1):
         # if n modules by root_value
         while not n % root_value:
             # add total_operation and root_value
             total_operation += root_value
-            # set n to the valoe of remainder
+            # set n to the value of remainder
             n /= root_value
-        # increment root by one until n lessthan 0
-        root_value += 1
+        if n == 1:
+            break
     return total_operation
