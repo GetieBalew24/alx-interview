1375f1a276274f761cc076cae09d5a92e96d757e
pc <getiebalew2020@gmail.com>
2023-04-05 15:22:56 +0300

    methods to calculate the fewest number of operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -4,12 +4,13 @@
 
 
 def minOperations(n):
-    if n <= 1:
-        return 0
-    dp = [float('inf')] * (n+1)
-    dp[1] = 0
-    for i in range(2, n+1):
-        for j in range(1, i):
-            if i % j == 0:
-                dp[i] = min(dp[i], dp[j] + i//j)
-    return dp[n] if dp[n] != float('inf') else 0
+    '''calculates the fewest number of operations needed.
+    '''
+    total_operation = 0
+    total_value = 2
+    while n > 1:
+        while not n % total_value:
+            total_operation += total_value
+            n /= total_value
+        total_value += 1
+    return total_operation
