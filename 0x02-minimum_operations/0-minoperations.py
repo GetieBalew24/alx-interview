532c419bd4881cac80c213db3f3415520b5f486a
pc <getiebalew2020@gmail.com>
2023-04-05 14:46:20 +0300

    methods to calculate the fewest number of operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" Module for 0-minoperationsIn a text file, there is a single character.
+    Your text editor can execute only two operations in this file: Copy 
+    All and Paste. Given a number n, write a method that calculates the 
+    fewest number of operations needed to result in exactly 
+    n H characters in the file."""
+
+
+def minOperations(n):
+    """ methods to calculate the fewest number of operations
+    """
+
+    # all outputs should be at least 2 char: (min, Copy All => Paste)
+    if (n < 2):
+        return 0
+    total_operation, root_value = 0, 2
+    while root_value <= n:
+
+        # if n modules root_value
+        if n % root_value == 0:
+
+            # the sum of total operation and root_value = total operations
+            total_operation += root_value
+
+            # set n to n divided by the root_value
+            n = n / root_value
+
+            # substract root_value by one to find the smaller values 
+            root_value -= 1
+
+        # increment root_value by one until it divides n
+        root_value += 1
+
+    return total_operation
