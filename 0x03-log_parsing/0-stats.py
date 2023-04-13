1c0872b5a061a4de6994a00e51f2aea9d617f5b8
GetieBalew24 <getiebalew2020@gmail.com>
2023-04-13 13:30:27 -0400

    display message

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -15,34 +15,31 @@ def print_message(dictonary_source, file_size):
     Returns:
         Nothing returns to caller func
     """
-
     print("File size: {}".format(file_size))
     for key, value in sorted(dictonary_source.items()):
         if value != 1:
             print("{}: {}".format(key, value))
-
-
 file_size = 0
 file_code = 0
-counter = 0
+count = 0
 dictonary_source = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
 try:
-    for line in sys.stdin:
-        parsed_line = line.split() 
+    for lines in sys.stdin:
+        parsed_line = lines.split() 
         parsed_line = parsed_line[::-1]  
 
         if len(parsed_line) > 2:
-            counter += 1
+            count += 1
 
-            if counter <= 10:
+            if count <= 10:
                 file_size += int(parsed_line[0])  
                 file_code = parsed_line[1]  
 
                 if (file_code in dictonary_source.keys()):
                     dictonary_source[file_code] += 1
 
-            if (counter == 10):
+            if (count == 10):
                 print_message(dictonary_source, file_size)
-                counter = 0
+                count = 0
 finally:
     print_message(dictonary_source, file_size)
