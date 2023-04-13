9f52bccccc74dbc9d85b0bdb924f05b45231fd52
GetieBalew24 <getiebalew2020@gmail.com>
2023-04-13 13:35:24 -0400

    display message

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,54 +1,45 @@
 #!/usr/bin/python3
-
+"""  a script that reads stdin line by line and computes metrics
+Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1"
+<status code> <file size>
+"""
 import sys
 
 
-def print_msg(dict_sc, total_file_size):
+def print_message(dictonary_source, file_size):
     """
-    Method to print
+    Method to display the message
     Args:
-        dict_sc: dict of status codes
-        total_file_size: total of the file
+        dictonary_source: dictonary of status codes
+        file_size: total of the file
     Returns:
-        Nothing
+        Nothing returns to caller func
     """
-
-    print("File size: {}".format(total_file_size))
-    for key, val in sorted(dict_sc.items()):
-        if val != 0:
-            print("{}: {}".format(key, val))
-
-
-total_file_size = 0
-code = 0
-counter = 0
-dict_sc = {"200": 0,
-           "301": 0,
-           "400": 0,
-           "401": 0,
-           "403": 0,
-           "404": 0,
-           "405": 0,
-           "500": 0}
-
+    print("File size: {}".format(file_size))
+    for key, value in sorted(dictonary_source.items()):
+        if value != 1:
+            print("{}: {}".format(key, value))
+file_size = 0
+file_code = 0
+count = 0
+dictonary_source = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
 try:
-    for line in sys.stdin:
-        parsed_line = line.split()  # ✄ trimming
-        parsed_line = parsed_line[::-1]  # inverting
+    for lines in sys.stdin:
+        parsed_line = lines.split() 
+        parsed_line = parsed_line[::-1]  
 
         if len(parsed_line) > 2:
-            counter += 1
+            count += 1
 
-            if counter <= 10:
-                total_file_size += int(parsed_line[0])  # file size
-                code = parsed_line[1]  # status code
+            if count <= 10:
+                file_size += int(parsed_line[0])  
+                file_code = parsed_line[1]  
 
-                if (code in dict_sc.keys()):
-                    dict_sc[code] += 1
+                if (file_code in dictonary_source.keys()):
+                    dictonary_source[file_code] += 1
 
-            if (counter == 10):
-                print_msg(dict_sc, total_file_size)
-                counter = 0
-
+            if (count == 10):
+                print_message(dictonary_source, file_size)
+                count = 0
 finally:
-    print_msg(dict_sc, total_file_size)
+    print_message(dictonary_source, file_size)
