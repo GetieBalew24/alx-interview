449d52d3de8c76466a94ba7700f185cf7ef6eace
GetieBalew24 <getiebalew2020@gmail.com>
2023-05-03 13:06:55 -0400

    starwars character

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+const request = require('request');
+const Film_Id = process.argv[2];
+const Movie_url = {
+  url: 'https://swapi-api.alx-tools.com/api/films/' + Film_Id,
+  method: 'GET'
+};
+
+request(Movie_url, function (error, response, body) {
+  if (!error) {
+    const chars = JSON.parse(body).characters;
+    printCharacters(chars, 0);
+  }
+});
+
+function printCharacters (chars, index) {
+  request(chars[index], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < chars.length) {
+        printCharacters(chars, index + 1);
+      }
+    }
+  });
+}
