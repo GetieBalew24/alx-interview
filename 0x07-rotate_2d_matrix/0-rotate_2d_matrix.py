a43d4add8302db0b9c65d9c66894f018bee71398
GetieBalew24 <getiebalew2020@gmail.com>
2023-05-11 11:03:00 -0400

    Readme File

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,25 +1,28 @@
 #!/usr/bin/python3
-"""2D matrix rotation module.
+"""
+n x n 2D matrix to rotate it 90 degrees clockwise.
 """
 
 
 def rotate_2d_matrix(matrix):
-    """Rotates an n by n 2D matrix in 90 degrees clockwise.
     """
-    if type(matrix) != list:
-        return
-    if len(matrix) <= 0:
-        return
-    if not all(map(lambda x: type(x) == list, matrix)):
-        return
-    n = len(matrix)
-    cols = len(matrix[0])
-    if not all(map(lambda x: len(x) == cols, matrix)):
-        return
-    # Transpose the matrix
-    for i in range(n):
-        for j in range(i, n):
-            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-    # Reverse each row
-    for i in range(n):
-        matrix[i] = matrix[i][::-1]
+    Args:
+        matrix (list): 2d square matrix
+    Return:
+        None
+    """
+    
+    
+    length = len(matrix)
+    for k in range(length):
+        for l in range(k):
+            temporary = matrix[k][l]
+            matrix[k][l] = matrix[l][k]
+            matrix[l][k] = temporary
+
+
+    for m in range(length):
+        for n in range(int(length / 2)):
+            temporary = matrix[m][n]
+            matrix[m][n] = matrix[m][length-1-n]
+            matrix[m][length-1-n] = temporary
